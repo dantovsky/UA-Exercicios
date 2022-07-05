c3d4159cc3260d8505e1effa43f5fac864d351c0
Dante Marinho <dantecnia@hotmail.com>
2022-07-05 17:44:02 +0100

    Reorganizing folders.

--- __lixo/projeto sawm frontend v1/src/components/EventItem.jsx
+++
@@ -1,8 +0,0 @@
-export function EventItem(props) {
-    return (
-        <li>
-            <strong>{props.event.name}</strong>
-            <p>{props.event.description}</p>
-        </li>
-    )
-}
